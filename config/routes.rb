f2207eb4ac5384df1f7b8523807d2391892f9867
Erik Artymiuk <eartymiuk@wisc.edu>
2014-02-24 17:14:38 -0600

    Finish Tests and BootStrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 P2::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/faq"
-  get "static_pages/about"
-  get "static_pages/blog"
+  root  'static_pages#home'
+  match '/blog',    to: 'static_pages#blog',    via: 'get'
+  match '/faq',   to: 'static_pages#faq',   via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,59 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+  subject { page }
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  describe "Home page" do
+    before { visit root_path }
+
+    let(:heading) { 'Project 2' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    let(:heading)    { 'About' }
+    let(:page_title) { 'About' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "Blog page" do
+    before { visit blog_path }
+
+    let(:heading)    { 'Blog' }
+    let(:page_title) { 'Blog' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "FAQ Page" do
+    before { visit faq_path }
+
+    let(:heading)    { 'FAQ' }
+    let(:page_title) { 'FAQ' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+    click_link "Blog"
+    expect(page).to have_title(full_title('Blog'))
+    click_link "FAQ"
+    expect(page).to have_title(full_title('FAQ'))
+    click_link "Home"
+    expect(page).to have_title(full_title(''))
   end
 end
